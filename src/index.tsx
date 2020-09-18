8ce020a35f1e321c4282faea0821f2ce0a81dda2
BearRebel <bearrebel@zooklabs.com>
2020-09-18 23:00:02 +0100

    add CookiesProvider and StoreProvider in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {CookiesProvider} from "react-cookie";
+import 'semantic-ui-css/semantic.min.css'
+import {createStore, Store} from 'redux'
+import {Provider as StoreProvider} from "react-redux";
+import {ApplicationState, rootReducer} from "./redux/rootReducer";
+import {devToolsEnhancer} from "redux-devtools-extension/index";
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+const store: Store<ApplicationState> = createStore(rootReducer, devToolsEnhancer({}));
+
+ReactDOM.render(
+    <React.StrictMode>
+        <CookiesProvider>
+            <StoreProvider store={store}>
+                <App/>
+            </StoreProvider>
+        </CookiesProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
