2a2d6a44ee2fc80987d1aa645c68b276f9c7f310
BearRebel <bearrebel@zooklabs.com>
2021-11-03 00:37:54 +0000

    attempt fix cors

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios, {AxiosInstance} from 'axios';
 const http: AxiosInstance = axios.create(
     {
         baseURL: `${process.env.REACT_APP_API_URL}/api`,
-        headers: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json'},
     });
 
 export default http
@@ -11,10 +11,12 @@ export default http
 export function removeAuthorization() {
     // @ts-ignore
     delete http.defaults.headers.common["Authorization"]
+    http.defaults.withCredentials = undefined
 }
 
 export function setAuthorization(token: string) {
     // @ts-ignore
     http.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    http.defaults.withCredentials = true
 }
 
